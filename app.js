63195932c4dfc6da6353830e405edd23ad1538d2
Dhruvin Sorathiya <108527154+DhruvinS720@users.noreply.github.com>
2023-02-18 14:44:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://cheery-entremet-1951d4.netlify.app",
   })
 );
 
